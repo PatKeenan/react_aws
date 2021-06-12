c0066a4dcce21d5d27212ae47048e021fd07426e
PatKeenan <patkeenan.dev@gmail.com>
2021-06-12 19:08:12 -0400

    testing rebuild feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React maybeee
+          Learn React maybe.....
         </a>
       </header>
     </div>
